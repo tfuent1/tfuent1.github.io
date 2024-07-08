eb8077bed6d71b08f625430976882bb25e289109
tfuent1 <Thomas.Fuentes@la.gov>
2024-07-08 12:46:07 -0500

    testing hyperlinks

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Project = ({ title, description, link }) => {
     return (
         <div className="project">
-            <h3 href={link}>{title}</h3>
+            <h3><a href={link}>{title}</a></h3>
             <p>{description}</p>
         </div>
     );
