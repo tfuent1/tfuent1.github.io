de09f50a9b25bb11d301f0fb1990d0b9bca2178b
tfuent1 <Thomas.Fuentes@la.gov>
2024-08-05 10:14:55 -0500

    Added about me description

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <section id="about">
             <h2>About Me</h2>
-            <p>Enter About Info</p>
+            <p>Welcome to my Portfolio Site! My name is Thomas Fuentes. I am a software developer with experience in many different languages such as Java, Python, Dart, JavaScript and TypeScript to name a few. I also have experience working with several frameworks for web and app development. These include Flutter, Angular, SpringBoot, React, and ExpressJS amongst others. I have always been interested in the latest and greatest of technology and I love getting the opportunity to dabble in new technologies. Furthermore, I am currently working towards getting my CompTIA Security+ to move into a more security focused professional environment! </p>
         </section>
     );
 };
