08f1c0e709d98d95d7f2d54fe79dc8ba127c9838
tfuent1 <Thomas.Fuentes@la.gov>
2024-08-05 11:54:00 -0500

    added image and styling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import photo from './assets/Engagement1.jpg'
 
 const App = () => {
     return (
         <div className="App">
             <Header />
-            <About />
+            <div id='AboutDiv'>
+                <About />
+                <img src={photo} alt='Photo' />
+            </div>
             <Projects />
             <Contact />
             <footer>
